f91927f955210fbf2252b704abd22138d8208789
Jordan Ereahn Tubianosa <jordantubs@Jordans-MacBook-Pro.local>
2026-09-25 19:30:24 +0800

    Publish PasaBai at new Pages path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/HitchPaymentTracker/",
+  base: "/Pasabai/",
   build: {
     outDir: "docs",
   },
